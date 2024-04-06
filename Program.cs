8ea0f8e5cab8172420701c706ca4c762eeca2146
chrisdfghjkl <cdepastene@gmail.com>
2024-04-05 21:07:08 -0700

    comment out and push challenge

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ int sisterEggs = dailyEggs / 4;
 int duckBearEggs = dailyEggs % 4;
 
 Console.WriteLine("Each sister receives " + sisterEggs + " eggs, and the remaining " + duckBearEggs + " eggs are for duckbear");
-*/
+
 
 //create a application that lets a user enter assortment of property: provinces, duchies, and estates. 
 //each property type adheres to point system - provinces - 6pt, duchies - 3pt, and estates - 1pt
@@ -34,6 +34,7 @@ int provinceScore = int.Parse(provinceInput) * 6;
 int holdingScore = estateScore + duchyScore + provinceScore;
 
 Console.WriteLine("You have a holding score of " + holdingScore + "!");
+*/
 
 
 
