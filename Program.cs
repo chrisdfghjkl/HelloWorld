98ec989ec2f763e1bd5bf3daaaf0081c25cee0bc
chrisdfghjkl <cdepastene@gmail.com>
2024-04-05 20:57:27 -0700

    add increment challenge directions

--- Program.cs
+++ Program.cs
@@ -12,3 +12,9 @@ int duckBearEggs = dailyEggs % 4;
 
 Console.WriteLine("Each sister receives " + sisterEggs + " eggs, and the remaining " + duckBearEggs + " eggs are for duckbear");
 */
+
+//create a application that lets a user enter assortment of property: provinces, duchies, and estates. 
+//each property type adheres to point system - provinces - 6pt, duchies - 3pt, and estates - 1pt
+//application should take 3 strings, convert them to numbers, add and return total
+
+
